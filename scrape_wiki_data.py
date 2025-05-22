23dd6bd923c9a7b2ada12fa3e072767c933b7626
Nandika K <2nandika.k@gmail.com>
2025-05-22 17:18:42 -0400

    various updates

--- scrape_wiki_data.py
+++ scrape_wiki_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sqlalchemy import create_engine
 #had to download lxml module for read_html
 
 #read all the tables from the page and store the first one in df
@@ -8,8 +9,21 @@ df = tables[0]
 #testing shows it reads it correctly
 #print(df['Symbol'].tolist())
 
-#store to csv file without indexing
-#df.to_csv('data/stocks.csv', index=False)
+# Create SQLAlchemy engine
+engine = create_engine('mysql+mysqlconnector://root:Database23!@127.0.0.1/fin_data_project')
+
+try:
+    df.to_sql('stocks', con=engine, if_exists='replace', index=False)
+    print("Data written successfully.")
+except Exception as e:
+    print(f"Error: {e}")
+finally:
+    engine.dispose()
+
+
+def update_stock_csv():
+    #store to csv file without indexing
+    df.to_csv('data/stocks.csv', index=False)
 
 def get_tickers():
     return df['Symbol'].tolist()
--- yahoo_fin_data.py
+++ yahoo_fin_data.py
@@ -1,17 +1,52 @@
 import yfinance as yf
 import scrape_wiki_data
+import pandas as pd
+import time
+import random
+ 
 
 cols = ['Ticker', 'Industry', 'Sub-Sector', 'Beta', 'Recommendation Score']
 data_lists = []
 
-for ticker in scrape_wiki_data.get_tickers():
-    #get the data
-    company_info = yf.Ticker(ticker)
-    beta = company_info.info.get('beta')
-    recommendations = company_info.get_recommendations();
-    print(ticker,beta, "\n", recommendations)
+recommendation_score_map = {
+    "Strong Buy": 4,
+    "Buy": 2,
+    "Hold": 1,
+    "Sell": -2,
+    "Strong Sell": -4
+}
 
-    #add data to list
-    data_lists.append([ticker, beta, rec_score])
+def load (lst, start, end):
+    for ticker in lst[start:end]:
+        if (index:=ticker.find('.')) != -1:
+            ticker = ticker[:index] + '-' + ticker[index+1:]
+        #get the data
+        company_info = yf.Ticker(ticker).info
+        beta = company_info.get('beta')
+        recommendations = company_info.recommendations;
+        
+        if recommendations is not None and not recommendations.empty:
+            print(recommendations)
+            #recommendation_score = recommendations.iloc[-1]['To Grade']
+            #recommendation_score = recommendation_score_map.get(recommendation_score, 0)
+        else:
+            recommendation_score = 0
 
-stock_data = pd.DataFrame(columns=cols)
+        #add data to list
+        data_lists.append([ticker, beta])
+
+start = 0
+end = 2
+full_list = scrape_wiki_data.get_tickers()
+print(full_list)
+load(full_list, start, end)
+
+while end <= 500:
+    
+    start += 10
+    end += 10
+    time.sleep(random.uniform(1, 3))
+
+stock_data = pd.DataFrame(columns=cols, data=data_lists)
+print(stock_data.head())
+
