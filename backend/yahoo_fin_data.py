e0201f54e7bc5fd6de6d7bc1b187074ed9421107
Nandika K <2nandika.k@gmail.com>
2025-05-24 20:23:21 -0400

    rate limited so will test tomorrow (hopefully)

--- backend/yahoo_fin_data.py
+++ backend/yahoo_fin_data.py
@@ -1,7 +1,5 @@
 import yfinance as yf
-from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table
-from sqlalchemy.orm import declarative_base, sessionmaker
-import backend.scrape_wiki_data as scrape_wiki_data
+from sqlalchemy import create_engine
 import pandas as pd
 import time
 import random
@@ -9,28 +7,8 @@ import traceback
 import os
 
 
-cols = ['Ticker', 'Industry', 'Sub-Sector', 'Beta', 'Recommendation Score']
-data_lists = []
-
 password = os.getenv("DB_PASSWORD")
-engine = create_engine('mysql+mysqlconnector://root:{password}@127.0.0.1/easy_s_and_p')
-Base = declarative_base()
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-
-class StockData(Base):
-    __tablename__ = 'stock_data'
-    id = Column(Integer, primary_key=True)
-    ticker = Column(String(10))
-    industry = Column(String(255), nullable=True)
-    sub_sector = Column(String(255), nullable=True)
-    beta = Column(String(50))
-    recommendation_score = Column(Integer)
-
-
-Base.metadata.create_all(engine)
+engine = create_engine(f'mysql+mysqlconnector://root:{password}@localhost/easy_s_and_p')
 
 recommendation_score_map = {
     "Strong Buy": 4,
@@ -40,58 +18,80 @@ recommendation_score_map = {
     "Strong Sell": -4
 }
 
-def load (lst, start, end):
-    for ticker in lst[start:end]:
-        if (index:=ticker.find('.')) != -1:
+def get_tickers():
+    #read tickers from sql table created by scrape_wiki_data
+    df = pd.read_sql('SELECT Symbol FROM stocks', con=engine)
+    return df['Symbol'].to_list()
+
+def fetch_data(ticker):
+    #Clean up names for any tickers with dots
+    if (index:=ticker.find('.')) != -1:
             ticker = ticker[:index] + '-' + ticker[index+1:]
-        #get the data
-        try:
-            tkr = yf.Ticker(ticker)
-            company_info = tkr.info
-            beta = company_info.get('beta', 'N/A')
-            recommendations = tkr.recommendations;
-            
-            if recommendations is not None and not recommendations.empty:
-                latest = recommendations.iloc[-1]
-                grade = latest.get('To Grade', None)
-                recommendation_score = recommendation_score_map.get(grade, 0)
-                print(recommendations)
-
-            #add data to list
-            data_lists.append([ticker, beta, recommendation_score])
-        except:
-            print("Error retrieving data for ticker: ", ticker)
-
-        try:
-            stock_row = StockData(
-                ticker=ticker,
-                beta=beta,
-                recommendation_score=recommendation_score,
-                industry=None,
-                sub_sector=None
-            )
-            session.add(stock_row)
-            session.commit()
-            print("Data added to SQL")
-        except Exception as e:
-            session.rollback()
-            print(f"Error occurred: {e}")
-            traceback.print_exc()
-        time.sleep(random.uniform(6, 10))
-
-start = 5
-end = 7
-full_list = scrape_wiki_data.get_tickers()
-#print(full_list)
-load(full_list, start, end)
-
-
-# while end <= 500:
     
-#     start += 10
-#     end += 10
-#     time.sleep(random.uniform(1, 3))
+    #get the data
+    try:
+        tkr = yf.Ticker(ticker)
+        company_info = tkr.info
 
-stock_data = pd.DataFrame(columns=cols, data=data_lists)
-print(stock_data.head())
+        #get beta
+        beta = company_info.get('beta', 'N/A')
 
+        #get recommendations
+        recommendations = tkr.recommendations;
+            
+        #compute recommendation score based on analyst recommendations
+        if recommendations is not None and not recommendations.empty:
+            latest = recommendations.iloc[-1]
+            grade = latest.get('To Grade', None)
+            recommendation_score = recommendation_score_map.get(grade, 0)
+            print(recommendations)
+        else:
+            recommendation_score = None
+
+        #return a list of the ticker, beta, and recommendation score
+        return {
+            "Ticker": ticker,
+            "Beta" : beta,
+            "Recommendation Score": recommendation_score
+        }
+    except:
+        #if there is an error, provide the details
+        print("Error retrieving data for ticker: ", ticker)
+        traceback.print_exc()
+        return None
+
+        
+
+def main():
+    #get list of tickers
+    tickers = get_tickers()
+    
+    #create list to store all the data lists
+    all_data = []
+    
+    #smaller amount for testing
+    
+    '''
+    iterate through tickers and fetch the data on all
+    if data is found, append list to all_data
+    '''
+    for ticker in tickers[0:5]:
+        data = fetch_data(ticker)
+
+        if data:
+            all_data.append(data)
+            print("Fetched data for ticker", ticker)
+
+    #load into pandas dataframe
+    df = pd.DataFrame(all_data)
+
+    try:
+        #load dataframe into an sql table called y_fin_data
+        df.to_sql('y_fin_data', con=engine, if_exists='replace', index=False)
+        print("Data written successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        engine.dispose()
+
+main()
