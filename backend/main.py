7f4d07f077a410cce1baca62fb9d6a504e67b1be
Nandika K <2nandika.k@gmail.com>
2025-05-27 09:06:57 -0400

    naming corrected

--- backend/main.py
+++ backend/main.py
@@ -6,41 +6,19 @@ import os
 #start FastAPI app
 app = FastAPI()
 
-'''
-CREATE TABLE s_and_p_stocks (
-	Symbol VARCHAR(100) NOT NULL,
-    Security VARCHAR(255) NOT NULL,
-    GICS_Sector VARCHAR(255),
-    GICS_Sub_Industry VARCHAR(255),
-    Headquarters_Location VARCHAR(255),
-    Date_Added DATE,
-    CIK INT NOT NULL,
-    Founded DATE,
-    PRIMARY KEY (Symbol)
-);
-
-CREATE TABLE yfin_ticker_info (
-	Ticker VARCHAR(100) NOT NULL,
-    Beta INT NOT NULL,
-    Recommendation_Score INT NOT NULL,
-    PRIMARY KEY (Ticker)
-);
-
-'''
-
 class WIKI_DATA(Base):
     __tablename__ = 's_and_p_stocks'
-    symbol = Column(String(100), primary_key=True)
-    security = Column(String(255), nullable=True)
-    sector = Column(String(100), nullable=True)
-    sub_industry = Column(String(100), nullable=True)
-    hq_location = Column(String(100), nullable=True)
-    date_added = Column(Date, nullable=True)
-    cik = Column(Integer)
-    date_founded = Column(Date, nullable=True)
+    Ticker = Column(String(100), primary_key=True)
+    Security = Column(String(255), nullable=True)
+    Sector = Column(String(100), nullable=True)
+    Sub_Industry = Column(String(100), nullable=True)
+    HQ_Location = Column(String(100), nullable=True)
+    Date_Added = Column(Date, nullable=True)
+    CIK = Column(Integer)
+    Founded = Column(Date, nullable=True)
 
 class YAHOO_FIN_DATA(Base):
     __tablename__ = 'yfin_ticker_info'
-    ticker = Column(String(100), primary_key=True)
-    beta = Column(Integer)
-    recommendation_score = Column(Integer)
+    Ticker = Column(String(100), primary_key=True)
+    bBeta = Column(Float)
+    Recommendation_Score = Column(Integer, nullable=True)
